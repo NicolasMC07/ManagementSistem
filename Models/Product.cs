674832fb9a8c1282b28ff1d204537c94e47abef9
NicolasMC07 <nicolasmarcar2006@gmail.com>
2024-10-10 10:10:36 -0500

    feat: model for product

---
+++ Models/Product.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Models
+{   [Table("product")]
+    public class Product
+    {   
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("price")]
+        public double Price { get; set; }
+
+        [Column("category_id")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public Category? Category { get; set; }
+
+        public Product(string name, string description, double price, int categoryId)
+        {
+            Name = name; Description = description; Price = price; CategoryId = categoryId;
+        }
+
+        public Product()
+        {
+        }
+    }
+}
